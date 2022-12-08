4756df12291bba304c95dd53f82ceb6a403bce9d
andreamusso96 <andreamusso96@gmail.com>
2022-12-08 14:38:50 +0100

    config changes

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
 
 is_cluster = False
+CODE_PATH = os.path.dirname(os.path.realpath(__file__))
+if "cluster" in CODE_PATH:
+    is_cluster = True
+
 if is_cluster:
-    absolute_path_to_db = 'PATH IN THE CLUSTER EVEN LONGER'
+    absolute_path_to_db = '/cluster/scratch/anmusso/PolarizationDB/cluster_sim_db.db'
 else:
     absolute_path_to_db = '/Users/anmusso/Desktop/PhD/Polarization/DB/sim_db.db'
 
