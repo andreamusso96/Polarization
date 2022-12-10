f971eae491df277a17b05647e06b4048e927b9b4
andreamusso96 <andreamusso96@gmail.com>
2022-12-10 11:10:25 +0100

    experimenting with simulation runner

--- SimulationRunner.py
+++ SimulationRunner.py
@@ -10,17 +10,16 @@ class SimulationRunner:
     @staticmethod
     def run_simulations():
         ids_sims_to_run = DB.get_ids_incomplete_simulations()
-        ids_sims_to_run = np.sort(np.random.choice(ids_sims_to_run, size=10))
         for sim_id in ids_sims_to_run:
             if is_cluster:
-                time_in_mins = 20
+                time_in_mins = 30
                 args_sim = ['sbatch', f'--time={time_in_mins}', f'--wrap="python -m main {str(sim_id)}"']
             else:
                 args_sim = ['python', '-m', 'main', str(sim_id)]
 
             os.system(" ".join(args_sim))
             print('SUBMITTED', sim_id, 'COMMAND', " ".join(args_sim))
-            time.sleep(1)
+            time.sleep(0.5)
 
 
 def check():
