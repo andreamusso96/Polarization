a2460de781d1a9a477849c9a61d3c9399b302f22
andreamusso96 <andreamusso96@gmail.com>
2023-02-21 11:33:00 +0100

    Job length

--- deleteSoon.py
+++ deleteSoon.py
@@ -1,8 +1,11 @@
 import os
+import numpy as np
 
 def run():
-    fnames=  np.load('fnames.npy')
+    fnames = np.load('fnames.npy')
     for fname in fnames:
         os.system(f'sacct --format=JobID,Elapsed,State --job {fname}')
 
 
+if __name__ == '__main__':
+    run()
